8fd45f6f923398b7976bd30165829430e6eeab62
Rik Goldman <ghoulmann@users.noreply.github.com>
2016-11-07 12:38:51 -0500

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,20 @@
 #!/usr/bin/env bash
 
 # Use single quotes instead of double quotes to make it work with special-character passwords
-PASSWORD='root123'
-#PROJECTFOLDER='myproject'
 
-# create project folder
-#sudo mkdir "/var/www/html/${PROJECTFOLDER}"
+PASSWORD='root123'
 
 # update / upgrade
 sudo apt-get update
-sudo apt-get -y upgrade
+sudo apt-get -y --force-yes upgrade
 
-# install apache 2.5 and php 5.5
-sudo apt-get install -y curl
-sudo apt-get install -y apache2
-sudo apt-get install -y php5
+# Install Packages
+sudo apt-get -y --force-yes install curl apache2 php5 php5-curl php-pear php5-gd imagemagick screen tmux vim curl git
 
 # install mysql and give password to installer
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server
-sudo apt-get install php5-mysql php5-curl php-pear php5-gd imagemagick
-sudo apt-get install screen tmux vim curl
+sudo apt-get install -y --force-yes mysql-server php5-mysql
 
 # install phpmyadmin and give password(s) to installer
 # for simplicity I'm using the same password for mysql and phpmyadmin
@@ -30,29 +23,13 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm pass
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get -y install phpmyadmin
-
-# setup hosts file
-VHOST=$(cat <<EOF
-<VirtualHost *:80>
-    DocumentRoot "/var/www/html"
-    <Directory "/var/www/html">
-        AllowOverride All
-        Require all granted
-    </Directory>
-</VirtualHost>
-EOF
-)
-echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+sudo apt-get install -y --force-yes phpmyadmin
 
 # enable mod_rewrite
 sudo a2enmod rewrite
 
 # restart apache
 service apache2 restart
-
-# install git
-sudo apt-get -y install git
 
 # install Composer
 curl -s https://getcomposer.org/installer | php
